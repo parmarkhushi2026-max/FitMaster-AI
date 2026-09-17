a975fa2865882dceb381e3877fba8e94923ff0bc
Khushi Parmar <parmarkhushi2026@gmail.com>
2026-09-17 09:50:18 +0530

    Secured admin dashboard, updated navbar dashboard links, and fixed dashboard visibility

--- FitMaster/urls.py
+++ FitMaster/urls.py
@@ -4,8 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('secure-backend/', admin.site.urls),
     path('', include('users.urls')),
-    path('django-admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
